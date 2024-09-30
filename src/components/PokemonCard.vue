a2edd8178f97f57867e0289a8de466d76982242e
worldrulesdev <worldrulesdev@gmail.com>
2024-09-30 10:15:15 -0300

    Criação da Lista e Card

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pokemon-card">
+    <h3>{{ pokemon.name }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    pokemon: {
+      type: Object as PropType<{ name: string, url: string }>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.pokemon-card {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  text-align: center;
+}
+</style>
---
+++ src/components/PokemonList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pokemon-list">
+    <PokemonCard
+      v-for="pokemon in pokemonList"
+      :key="pokemon.name"
+      :pokemon="pokemon"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue';
+import { usePokemon } from '@/composables/usePokemon';
+import PokemonCard from './PokemonCard.vue';
+
+export default defineComponent({
+  components: { PokemonCard },
+  setup() {
+    const { pokemonList, getPokemonList } = usePokemon();
+
+    onMounted(() => {
+      getPokemonList();
+    });
+
+    return {
+      pokemonList,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.pokemon-list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+}
+</style>
