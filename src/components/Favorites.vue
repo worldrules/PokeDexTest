7586927255c0ebd30f7a8240b79ac3de08c6c3f1
worldrulesdev <worldrulesdev@gmail.com>
2024-10-01 18:36:52 -0300

    work 1/2

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -3,7 +3,7 @@
         <h1>Pokémon Favoritos</h1>
         <div class="pokemon-list" v-if="favoritePokemons.length">
             <PokemonCard v-for="(pokemon, index) in favoritePokemons" :key="index" :pokemon="pokemon"
-                :pokemonId="getPokemonId(pokemon.url)" :isFavorite="true" />
+                :pokemonId="getPokemonId(pokemon.url)" :isFavorite="true" @toggle-favorite="toggleFavorite" />
         </div>
         <div v-else class="no-results">
             Nenhum Pokémon favorito encontrado.
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import PokemonCard from '@/components/PokemonCard.vue';
 
 export default defineComponent({
@@ -20,16 +20,29 @@ export default defineComponent({
         PokemonCard,
     },
     setup() {
-        const favoritePokemons = JSON.parse(localStorage.getItem('favoritePokemons') || '[]');
+        const favoritePokemons = ref(JSON.parse(localStorage.getItem('favoritePokemons') || '[]'));
 
         const getPokemonId = (url: string) => {
+            if (!url) return ''; // Verifica se a URL está definida
             const segments = url.split('/');
             return segments[segments.length - 2];
+        };
+
+        const toggleFavorite = (id: string) => {
+            const currentFavorites = favoritePokemons.value;
+            const index = currentFavorites.findIndex((pokemon: any) => getPokemonId(pokemon.url) === id);
+            if (index !== -1) {
+                currentFavorites.splice(index, 1);
+            } else {
+            }
+            localStorage.setItem('favoritePokemons', JSON.stringify(currentFavorites));
+            favoritePokemons.value = currentFavorites;
         };
 
         return {
             favoritePokemons,
             getPokemonId,
+            toggleFavorite,
         };
     },
 });
